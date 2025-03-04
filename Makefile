b5959e9e7b2aa61d02c42fab2e8aa36c77ca766d
dimend <diogomedeirosmendonca@enta.pt>
2025-03-04 10:57:43 +0200

    added pid handler, find envp path

--- Makefile
+++ Makefile
@@ -1,34 +1,35 @@
 CC = cc
-
-CFLAGS = -Wall -Wextra -Werror -g -I./includes
+CFLAGS = -Wall -Wextra -Werror -g -I./includes -I./src/libft
 
 TARGET = pipex
 
 OBJ_PATH = obj/
+LIBFT_DIR = src/libft
+LIBFT_LIB = $(LIBFT_DIR)/libft.a
 
 SRCS =	src/pipex.c
-
 OBJS = $(SRCS:src/%.c=$(OBJ_PATH)%.o)
-
 HEADERS = includes/pipex.h
 
 $(OBJ_PATH)%.o: src/%.c $(HEADERS)
 	@mkdir -p $(dir $@)
 	$(CC) $(CFLAGS) -c $< -o $@
 
-all: $(TARGET)
+all: $(LIBFT_LIB) $(TARGET)
 
-$(TARGET): $(OBJS)
-	$(CC) $(CFLAGS) $(OBJS) -o $(TARGET)
+$(TARGET): $(OBJS) $(LIBFT_LIB)
+	$(CC) $(CFLAGS) $(OBJS) $(LIBFT_LIB) -o $(TARGET)
 
-%.o: %.c $(HEADERS)
-	$(CC) $(CFLAGS) -c $< -o $@
+$(LIBFT_LIB):
+	$(MAKE) -C $(LIBFT_DIR)
 
 clean:
 	rm -f $(OBJS)
+	rm -rf $(OBJ_PATH)
 
-fclean: 
-	rm -f $(OBJS) $(TARGET)
+fclean: clean
+	rm -f $(TARGET)
+	$(MAKE) -C $(LIBFT_DIR) fclean
 
 re: fclean all
 
--- src/pipex.c
+++ src/pipex.c
@@ -1,73 +1,118 @@
 #include "pipex.h"
+#include "libft/libft.h"
 #include <stdio.h>
 
-void error_exit(char *msg)
+void error()
 {
-    perror(msg);
+    perror("Error");
     exit(EXIT_FAILURE);
 }
 
-void pid_handler(pid_t pid, int pipefd[2], short int read_end, char *cmd)
+char **get_all_paths(char **envp)
 {
-    if (pid == 0)
+    int i;
+    char **paths;
+
+    i = 0;
+    while(envp[i])
     {
-        char *cmds[] = {cmd, "hello", NULL};
-
-        if (read_end == 1)
+        if(envp[i][0] == 'P' && envp[i][1] == 'A' && envp[i][2] == 'T' && envp[i][3] == 'H')
         {
-            close(pipefd[0]);
-            dup2(pipefd[1], 1);
-            close(pipefd[1]);
-        }
-        else if (read_end == 0)
-        {
-            close(pipefd[1]);
-            dup2(pipefd[0], 0);
-            close(pipefd[0]);
-        }
+            paths = ft_split(envp[i] + 5, ':');
+            break;
+        } 
+        i++;
+    }
+    return (paths);
+}
 
-        if (execvp(cmds[0], cmds) == -1)
-            error_exit("Execvp failed for ");
+char *get_path(char **envp, char *cmd)
+{
+    int i;
+    char **paths;
+    char *finalpath;
+
+    paths = get_all_paths(envp);
+    i = 0;
+    while(paths[i])
+    {
+        paths[i] = ft_strjoin(paths[i], "/");
+        paths[i] = ft_strjoin(paths[i], cmd);
+        if (access(paths[i], F_OK) == 0)
+        {
+            finalpath = ft_strdup(paths[i]);
+            i = 0;
+            while (paths[++i])
+			    free(paths[i]);
+            return (finalpath);
+        }
+        i++;
+    }
+    return (NULL);
+}
+
+void execute(char *argV, char **envp)
+{
+	char	**cmd;
+	int 	i;
+	char	*path;
+	
+	i = -1;
+	cmd = ft_split(argV, ' ');
+	path = get_path(envp, cmd[0]);
+	if (!path)	
+	{
+		while (cmd[++i])
+			free(cmd[i]);
+		free(cmd);
+		error();
+	}
+	if (execve(path, cmd, envp) == -1)
+		error();
+}
+
+void pid_handler(char **argV, char **envp, int *pipefd, short int read)
+{
+	int		file;
+
+    if(read == 0)
+    {
+        file = open(argV[1], O_RDONLY, 777);
+        dup2(pipefd[1], STDOUT_FILENO); //dup pipefd and assign stdout so that stdout goes to the pipe
+	    dup2(file, STDIN_FILENO);       //dup file and assign stdin to read from file and not stdin
+        if (file == -1)
+		    error();
+	    close(pipefd[0]);
+	    execute(argV[2], envp);
+    }
+    else
+    {
+	    file = open(argV[4], O_WRONLY | O_CREAT | O_TRUNC, 777);
+	    if (file == -1)
+		    error();
+	    dup2(pipefd[0], STDIN_FILENO);  //dup pipefd to stin to read from pipe
+	    dup2(file, STDOUT_FILENO);      //dup file to stdout so it writes to file
+	    close(pipefd[1]);
+	    execute(argV[3], envp);
     }
 }
 
 int main(int argC, char **argV, char **envp) 
 {
     int pipefd[2];
-    pid_t pid1, pid2;
-    int i = 0;
+    pid_t pid;
 
-    if (pipe(pipefd) == -1)
-        error_exit("Error creating pipe");
-
-    pid1 = fork();
-    if (pid1 == -1)
-        error_exit("Error forking first process");
-
-    if (pid1 == 0)
-        pid_handler(pid1, pipefd, 1, "echo");
-
-    pid2 = fork();
-    if (pid2 == -1)
-        error_exit("Error forking second process");
-    if (pid2 == 0)
-        pid_handler(pid2, pipefd, 0, "grep");
-
-    close(pipefd[0]);
-    close(pipefd[1]);
-
-    printf("%d\n", argC);
-    printf("%s\n", argV[i]);
-
-    while(envp[i])
+    if(argC == 5)
     {
-        if(envp[i][0] == 'P' && envp[i][1] == 'A' && envp[i][2] == 'T' && envp[i][3] == 'H')
-            printf("%s\n", envp[i]);
-        i++;
+        if(pipe(pipefd) == -1)
+            error();
+        pid = fork();
+        if(pid == -1)
+            error();
+        if(pid == 0)
+            pid_handler(argV, envp, pipefd, 0); //child
+        waitpid(pid, NULL, 0);
+        pid_handler(argV, envp, pipefd, 1);     //parent
     }
-
-    wait(NULL);
-    wait(NULL);
-
     return 0;
 }
