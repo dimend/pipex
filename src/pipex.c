419469e02de4037e8c7d30f84bebac167faf1741
dimendon <diogomedeirosmendonca@enta.pt>
2025-03-18 18:23:57 +0200

    added 1 more child

--- src/pipex.c
+++ src/pipex.c
@@ -50,72 +50,81 @@ void execute(char *argV, char **envp)
     free(command);
 }
 
-void pid_handler(char **argV, char **envp, int *pipefd, short int read)
+//read from infile, write to pipe
+void readchild(char **argV, char **envp, int *pipefd)
 {
-	int		file;
+    int		file;
     char    *filename;
-    
-    checkargs(argV[1], argV[2]);
 
-    if(read == 0)
-    {
         // dup2(file, STDIN_FILENO);
         // close(pipefd[0]);
         // close(pipefd[1]);
         // check permission,
         // handle the redirector,
         // exec cmd.
-        checkargs(argV[1], argV[2]);
-        filename = ft_strdup(argV[1]);
-        close(pipefd[0]);
-        file = open(filename, O_RDONLY, 0777);
-        if (file == -1)
-		    error(filename, "perror", 1);
-        dup2(pipefd[1], STDOUT_FILENO); //dup pipefd and assign stdout so that stdout goes to the pipe
-        close(pipefd[1]);
-	    dup2(file, STDIN_FILENO);       //dup file and assign stdin to read from file and not stdin
-        close(file);
-        free(filename);
-	    execute(argV[2], envp);
-    }
-    else
-    {
-        // close(pipefd[1]);
-        // waitpid(pid, 0, NULL);
-        checkargs(argV[4], argV[3]);
-        filename = ft_strdup(argV[4]);
-	    file = open(argV[4], O_WRONLY | O_CREAT | O_TRUNC, 0777);
-	    if (file == -1)
-		    error(filename, "perror", 1);
-        close(pipefd[1]);
-	    dup2(pipefd[0], STDIN_FILENO);  //dup pipefd to stin to read from pipe
-        close(pipefd[0]);
-	    dup2(file, STDOUT_FILENO);      //dup file to stdout so it writes to file
-        close(file);
-        free(filename);
-	    execute(argV[3], envp);
-    }
+    checkargs(argV[1], argV[2]);
+    filename = ft_strdup(argV[1]);
+    close(pipefd[0]);
+    file = open(filename, O_RDONLY, 0777);
+    if (file == -1)
+		error(filename, "perror", 1);
+    dup2(pipefd[1], STDOUT_FILENO); //dup pipefd and assign stdout so that stdout goes to the pipe
+    close(pipefd[1]);
+    dup2(file, STDIN_FILENO);       //dup file and assign stdin to read from file and not stdin
+    close(file);
+    free(filename);
+	execute(argV[2], envp);
 }
+
+//read from pipe write to outfile
+void writechild(char **argV, char **envp, int *pipefd)
+{
+    int		file;
+    char    *filename;
+
+    checkargs(argV[4], argV[3]);
+    filename = ft_strdup(argV[4]);
+	file = open(argV[4], O_WRONLY | O_CREAT | O_TRUNC, 0777);
+	if (file == -1)
+	    error(filename, "perror", 1);
+    close(pipefd[1]);
+    dup2(pipefd[0], STDIN_FILENO);  //dup pipefd to stin to read from pipe
+    close(pipefd[0]);
+	dup2(file, STDOUT_FILENO);      //dup file to stdout so it writes to file
+    close(file);
+    free(filename);
+	execute(argV[3], envp);
+}
+
 
 int main(int argC, char **argV, char **envp) 
 {
     int pipefd[2];
-    pid_t pid;
+    pid_t pid1, pid2;
 
     if(argC == 5)
     {
         if(pipe(pipefd) == -1)
             error(argV[1], "pipefd1", 1);
 
-        pid = fork();
-        if(pid == -1)
-            error(argV[4], "pipefd2", 1);
+        pid1 = fork();
+        if(pid1 == -1)
+            error(argV[1], "fork1", 1);
 
-        if(pid == 0)
-            pid_handler(argV, envp, pipefd, 0); //child
+        if(pid1 == 0)
+            readchild(argV, envp, pipefd);
+
+        pid2 = fork();
+        if(pid2 == -1)
+            error(argV[4], "fork2", 1);
         
-        waitpid(pid, NULL, 0);
-        pid_handler(argV, envp, pipefd, 1);     //parent
+        if(pid2 == 0)
+            writechild(argV, envp, pipefd);
+
+        close(pipefd[0]);
+        close(pipefd[1]);
+        waitpid(pid1, NULL, 0);
+        waitpid(pid2, NULL, 0);
         // pid1 = fork();
         // if (pid == 0) { exit()}
         // close(pipefd[1]);
@@ -125,7 +134,6 @@ int main(int argC, char **argV, char **envp)
         // waitpid(pid1);
         // waitpid(pid2);
         // exit();
-
     }
     else
         error(NULL, "Usage: ./pipex infile cmd1 cmd2 outfile", 1);
